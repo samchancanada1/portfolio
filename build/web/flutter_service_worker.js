2a1b7655dda0ae5bdbbebf84d99c207c10172405
samchan <samchancanada1@gmail.com>
2025-01-07 05:27:04 -0500

    add build

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,230 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"flutter_bootstrap.js": "677c6185ebaeb3968c5f0f6587159a98",
+"version.json": "009c9e65172e010890f7f65fde438006",
+"index.html": "80b7f3c2a7bb29c643595523dbc0fc2e",
+"/": "80b7f3c2a7bb29c643595523dbc0fc2e",
+"main.dart.js": "b685274583e61b48c3def6d22b2d5917",
+"flutter.js": "4b2350e14c6650ba82871f60906437ea",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "d40c47d1c161f94dbcb13094d37f1f55",
+"main.dart.js_1.part.js": "769ac8594612d20024035666023914c7",
+"assets/AssetManifest.json": "12a4fd08108651670e0496c647bfb292",
+"assets/NOTICES": "cace99041b52e5e03ea613a7feeb363d",
+"assets/pubspec.yaml": "8bf5f2fa4283f6b70574453f74fea9b8",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/AssetManifest.bin.json": "de37d73de4d48a448e0cb5936f33b38a",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.bin": "436a0b7f0caa3c2cdeaa0c68e12ef921",
+"assets/fonts/MaterialIcons-Regular.otf": "bb67fe3535f2bc37889b5a62296808ce",
+"assets/assets/images/profile-image.jpg": "1c92a1a245b15e9c2c0dbce0177555b4",
+"assets/assets/images/vr_image.png": "ba4c48c127365724778444a0b0ac5d7e",
+"assets/assets/images/cv_image.png": "67412900e8881522be26127e83a179e5",
+"assets/assets/images/mall_image.png": "5937673f315453fcd2fc971b7b46e23b",
+"assets/assets/images/splash.jpg": "e91e3b0266457fe11817d429d803f8b6",
+"assets/assets/images/home-background.jpg": "57997001c478533b84897a2e35309898",
+"assets/assets/images/pos_image.png": "ae7d838a7a0dea82e9af0ccacbe5cefb",
+"assets/assets/images/tung-logo.svg": "2b14bf68ef47b9b086b913714a3c08dd",
+"assets/assets/images/small-home-bg.jpg": "ef6f8ded37c9c727d62cda19cfd224db",
+"assets/assets/icons/cloack.svg": "a8e58e2e46c7628627991540722b0cc1",
+"assets/assets/icons/mall_icon.svg": "9d4118193d9fea7ca2d6459821ebc5d9",
+"assets/assets/icons/cv_icon.svg": "2af988f91254b49d7723af4d51b73117",
+"assets/assets/icons/github_logo.png": "d873982c0b41fdfc16902856d9c75a66",
+"assets/assets/icons/linkedin_icon.png": "54e088a4b4cc32e59f72798ac8bd3b8c",
+"assets/assets/icons/whatsapp_logo.png": "037049ec81e8f3c1245fbffeac39bcd3",
+"assets/assets/icons/projects-list.svg": "6b4f3d0174db155b85884bba185812c3",
+"assets/assets/icons/email_icon.png": "d917973cfab718140ab68ebca1683f50",
+"assets/assets/icons/vr_icon.svg": "efd70c8b6610100db05af77b56ed3ff5",
+"assets/assets/icons/youtube_logo.png": "1d41f85bc2e3714b4dc02954d905350b",
+"assets/assets/icons/pos_icon.svg": "fa6a5525bc9118cf76dd8d08254cf110",
+"assets/assets/icons/stackoverflow_icon.png": "8ef16ef9571e0f3f5310ab5ee81502b5",
+"assets/assets/icons/users.svg": "9b39d8f56dce8734f39e9c5606d012ac",
+"assets/assets/animations/not-found.json": "9eb1b73715e1761888f0ea5930fbc753",
+"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
+"canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
+"canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
+"canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
+"canvaskit/chromium/canvaskit.js.symbols": "e115ddcfad5f5b98a90e389433606502",
+"canvaskit/chromium/canvaskit.js": "b7ba6d908089f706772b2007c37e6da4",
+"canvaskit/chromium/canvaskit.wasm": "ea5ab288728f7200f398f60089048b48",
+"canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
+"canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ build/web/main.dart.js_1.part.js
@@ -0,0 +1,129 @@
+((a,b,c)=>{a[b]=a[b]||{}
+a[b][c]=a[b][c]||[]
+a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
+aPH(d,e,f){var w=new B.xp(C.ka,d),v=new A.X3(w)
+w.r=A.b6E(v)
+return v},
+b6C(d,e){var w="Fran\xe7ais"
+switch(e){case"mainScreen.title":return"Le titre fran\xe7ais"
+case"mainScreen.counter":return new A.aDj(d)
+case"mainScreen.tapMe":return"Appuyez-moi"
+case"locales.en":return"Anglais"
+case"locales.fr":return w
+case"notFound":return"Page introuvable"
+case"home_screen.home":return"D'accueil"
+case"home_screen.about":return"\xc0 propos de"
+case"home_screen.resume":return"Reprendre"
+case"home_screen.skills":return"Comp\xe9tences"
+case"home_screen.contact":return"Contacter"
+case"home_screen.settings":return"Param\xe8tres"
+case"home_screen.myName":return"Hiu Tung Chan"
+case"home_screen.iAm":return"Je suis"
+case"home_screen.developer":return"D\xe9veloppeur"
+case"home_screen.designer":return"Designer"
+case"home_screen.freelancer":return"Pigiste"
+case"home_screen.aboutTitle":return y.p
+case"home_screen.aboutMeDescription":return y.n
+case"home_screen.statistics":return"Statistiques"
+case"home_screen.website":return"Site Web"
+case"home_screen.phone":return"Num\xe9ro de t\xe9l\xe9phone"
+case"home_screen.city":return"Ville"
+case"home_screen.myCity":return"Toronto, ON, CA"
+case"home_screen.degree":return"Degr\xe9"
+case"home_screen.myDegree":return y.o
+case"home_screen.email":return"Courriel"
+case"home_screen.freelance":return"Ind\xe9pendant"
+case"home_screen.available":return"Disponible"
+case"home_screen.happyClients":return"Clients satisfaits"
+case"home_screen.projects":return"Projects"
+case"home_screen.trainedStudent":return"\xc9tudiants form\xe9s"
+case"home_screen.hoursOfSupport":return"heures d'assistance"
+case"home_screen.view":return"vue"
+case"home_screen.coreSkills":return"Comp\xe9tences de base"
+case"home_screen.allSkills":return"Toutes les comp\xe9tences"
+case"home_screen.french":return w
+case"home_screen.english":return"Anglais"
+case"home_screen.themeMode":return"Mode th\xe8me"
+case"home_screen.themeColor":return"Couleur du th\xe8me"
+case"home_screen.language":return"Langue"
+default:return null}},
+b6E(d){return new A.aDl(d)},
+X3:function X3(d){this.a=d
+this.d=this.b=$},
+aCi:function aCi(){},
+aDl:function aDl(d){this.a=d},
+aDj:function aDj(d){this.a=d}}
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[3],A)
+A.X3.prototype={
+i(d,e){return this.a.M5(e)},
+gT5(){var w=this.b
+if(w===$){w!==$&&B.aa()
+w=this.b=this}return w},
+gKd(){var w=x.c
+return B.ay(["en","Anglais","fr","Fran\xe7ais"],w,w)},
+gZw(){return"Page introuvable"},
+gc6(){var w=this,v=w.d
+if(v===$){w.gT5()
+w.d!==$&&B.aa()
+v=w.d=new A.aCi()}return v},
+$iil:1,
+$iiz:1,
+gxI(){return this.a}}
+A.aCi.prototype={
+gJN(){return"D'accueil"},
+gA9(){return"\xc0 propos de"},
+gCN(){return"Reprendre"},
+gyf(){return"Comp\xe9tences"},
+gy9(){return"Param\xe8tres"},
+gKn(){return"Hiu Tung Chan"},
+gYG(){return"Je suis"},
+gXj(){return"D\xe9veloppeur"},
+gXi(){return"Designer"},
+gY8(){return"Pigiste"},
+gVG(){return y.p},
+gVF(){return y.n},
+gML(){return"Statistiques"},
+ga0q(){return"Site Web"},
+gZP(){return"Num\xe9ro de t\xe9l\xe9phone"},
+gWz(){return"Ville"},
+gZs(){return"Toronto, ON, CA"},
+gXe(){return"Degr\xe9"},
+gZt(){return y.o},
+gXH(){return"Courriel"},
+gY7(){return"Ind\xe9pendant"},
+gWd(){return"Disponible"},
+gYq(){return"Clients satisfaits"},
+gZY(){return"Projects"},
+ga_V(){return"\xc9tudiants form\xe9s"},
+gYF(){return"heures d'assistance"},
+ga04(){return"vue"},
+gX_(){return"Comp\xe9tences de base"},
+gVT(){return"Toutes les comp\xe9tences"},
+gY9(){return"Fran\xe7ais"},
+gXM(){return"Anglais"},
+ga_F(){return"Mode th\xe8me"},
+ga_E(){return"Couleur du th\xe8me"},
+gK6(){return"Langue"}}
+var z=a.updateTypes([])
+A.aDl.prototype={
+$1(d){return A.b6C(this.a,d)},
+$S:76}
+A.aDj.prototype={
+$1$n(d){var w
+this.a.gT5()
+w=B.aOb("fr")
+return w.$3$one$other(d,"Vous avez appuy\xe9 une fois.","Vous avez appuy\xe9 "+B.j(d)+" fois.")},
+$C:"$1$n",
+$R:0,
+$D(){return{n:C.hi}},
+$S:217};(function inheritance(){var w=a.inheritMany
+w(B.D,[A.X3,A.aCi])
+w(B.lB,[A.aDl,A.aDj])})()
+B.aQJ(b.typeUniverse,JSON.parse('{"X3":{"il":[],"iz":["hI","il"]}}'))
+var y={o:"Bachelor d'ing\xe9nieur en g\xe9nie informatique",p:"D\xe9veloppeur Flutter et sp\xe9cialiste des applications mobiles",n:"D\xe9veloppeur Flutter exp\xe9riment\xe9 avec 4 ans d'exp\xe9rience dans le d\xe9veloppement d'applications mobiles, fournissant des applications hautes performances centr\xe9es sur l'utilisateur. Comp\xe9tent dans la conception frontale, la mise en \u0153uvre de fonctionnalit\xe9s et l'optimisation de l'\xe9volutivit\xe9 pour r\xe9pondre \xe0 divers objectifs commerciaux."}
+var x={c:B.a6("f")}};
+((a,b)=>{a[b]=a.current
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"L7i8oNAPasbbSWuYa2yCvuQi76Y=");
